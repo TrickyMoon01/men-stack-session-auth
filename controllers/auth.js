1da917fdd0ed47a219f8d29657bb0a1c98ee5979
JamesYon <trickymoon01@gmail.com>
2025-07-22 12:43:11 -0400

    create user functionality complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,31 @@
 const express = require('express');
-const router = express.Router();
+const bcrypt = require('bcrypt');
 
-router.get('/sign-up', async (req, res) => {
+const router = express.Router();
+const User = require('../models/user.js');
+
+router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs');
+});
+
+router.post('/sign-up', async (req, res) => {
+    const userInDataBase = await User.findOne({ username: req.body.username });
+
+    if (userInDataBase) {
+        return res.send('username already taken');
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send('Password and Confirm password must match');
+    }
+    
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+    // hashedPassword is holding the value that comes back from bcrypt running a hashSync method on the password
+    
+    const user = await User.create(req.body);
+
+    res.send(`Thanks for signing up ${user.username}`);
 });
 
 module.exports = router;
