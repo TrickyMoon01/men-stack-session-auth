7b4db604a8544fbde6397c9c62a3483d2703dcb9
JamesYon <trickymoon01@gmail.com>
2025-07-22 11:53:22 -0400

    establish controller folder

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
+const authController = require('./controllers/auth.js');
+
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -24,10 +26,11 @@ app.use(methodOverride("_method"));
 app.use(morgan('dev'));
 
 
-app.get('/', async (requestAnimationFrame, res) => {
+app.get('/', async (req, res) => {
     res.render('index.ejs');
 });
 
+app.use('/auth', authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
